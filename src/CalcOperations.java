c584418e0b78a40719866fae0e987c0b5161f3b8
Vladyslav Yakymenko <vladyslav_yakymenko@epam.com>
2016-06-01 16:44:08 +0300

    Initial commit

---
+++ src/CalcOperations.java
@@ -0,0 +1,24 @@
+/**
+ * Created by Vlad on 6/1/2016.
+ */
+public class CalcOperations {
+    double add(double a, double b) {
+        double answer = a+b;
+        return answer;
+    }
+
+    double subtract(double a, double b) {
+        double answer = a-b;
+        return answer;
+    }
+
+    double multiply(double a, double b) {
+        double answer = a*b;
+        return answer;
+    }
+
+    double divide(double a, double b) {
+        double answer = a/b;
+        return answer;
+    }
+}
---
+++ src/Calculator.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ * Created by Vlad on 6/1/2016.
+ */
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Calculator ready for use");
+        Scanner input = new Scanner(System.in);
+
+        CalcOperations calcOperations = new CalcOperations();
+
+        double answer;
+        double inputX = input.nextDouble();
+        char operator = input.next().charAt(0);
+        double inputY = input.nextDouble();
+
+        if (operator == '+'){
+            answer = CalcOperations.
+        }
+
+    }
+}
